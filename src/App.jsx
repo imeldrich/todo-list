bb9bf5683a64ea107447a397c0bbe1495f1f8bf6
Eldrich Salonga <eldrich.salonga@gmail.com>
2026-05-03 23:23:20 +0800

    add a checkbox and local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 import deleteImg from "./assets/delete.png";
 import editImg from "./assets/edit.png";
 import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    const tasksFromDB = localStorage.getItem("tasks");
+    return tasksFromDB ? JSON.parse(tasksFromDB) : [];
+  });
   const [newTask, setNewTask] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const handleInputChange = (e) => {
     setNewTask(e.target.value);
@@ -14,20 +22,36 @@ function App() {
   const addTask = () => {
     if (newTask) {
       setTasks((tasks) => {
-        return [...tasks, newTask];
+        return [...tasks, { id: nanoid(), text: newTask, completed: false }];
       });
       setNewTask("");
     }
   };
 
-  const deleteTask = (index) => {
-    const updatedTasks = tasks.filter((task, i) => i !== index);
+  const deleteTask = (id) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
     setTasks(updatedTasks);
+  };
+
+  const handleCheck = (id) => {
+    const updatedTask = tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          completed: !task.completed,
+        };
+      } else {
+        return task;
+      }
+    });
+
+    updatedTask.sort((a, b) => a.completed - b.completed);
+    setTasks(updatedTask);
   };
 
   return (
     <div className="bg-[#cccccc] min-h-screen p-5 flex items-center justify-center select-none">
-      <div className="bg-[#f2f2f2] w-3xl h-fit rounded-xl p-5 flex flex-col items-center gap-5 shadow-lg font-sans tracking-wider">
+      <div className="bg-[#f2f2f2] w-3xl h-fit rounded-xl p-6 flex flex-col items-center gap-5 shadow-lg font-mono tracking-wider">
         <h1 className="text-[#0b2545] text-3xl font-bold">To-Do-List</h1>
         <div className="flex gap-3">
           <input
@@ -51,24 +75,39 @@ function App() {
         </div>
         <div>
           {tasks.length > 0 ? (
-            tasks.map((task, index) => {
+            tasks.map((task) => {
               return (
-                <div className="bg-white w-lg px-3 py-2 rounded-md shadow-xs mb-2 flex justify-between items-center">
-                  <p>{task}</p>
+                <div
+                  key={task.id}
+                  className="bg-white w-lg px-3 py-2 rounded-md shadow-xs mb-2 flex justify-between items-center"
+                >
+                  <input
+                    type="checkbox"
+                    id="task"
+                    onChange={() => handleCheck(task.id)}
+                    checked={task.completed}
+                  />
+                  <label
+                    className={
+                      task.completed ? "line-through text-zinc-400" : ""
+                    }
+                  >
+                    {task.text}
+                  </label>
                   <div className="flex items-center gap-2.5">
                     <img src={editImg} alt="edit image" className="h-6" />
                     <img
                       src={deleteImg}
                       alt="delete image"
                       className="h-6"
-                      onClick={() => deleteTask(index)}
+                      onClick={() => deleteTask(task.id)}
                     />
                   </div>
                 </div>
               );
             })
           ) : (
-            <p className="text-slate-500 text-base">
+            <p className="text-zinc-500 text-base">
               You don't have a task yet...
             </p>
           )}
